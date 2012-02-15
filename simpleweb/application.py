de818284bc904b87ee86358a8d41b94aacedce24
Jacob Kupersmith <jakerosoft@gmail.com>
2012-02-15 12:41:22 -0500

    Explicitly set http status and content-type for every request

--- simpleweb/application.py
+++ simpleweb/application.py
@@ -26,15 +26,18 @@ class application(object):
             try:
                 web.ctx.doc.widgets.insert(0, widget(*m.groups()))
                 response = web.ctx.doc.render()
+                web.ctx.status = '200 OK'
+                web.ctx.headers['Content-Type'] = 'text/html'
             except:
                 import traceback
-                web.ctx.headers = {'Content-Type': 'text/plain'}
+                web.ctx.status = '500 Internal Server Error'
+                web.ctx.headers['Content-Type'] = 'text/plain'
                 response = traceback.format_exc()
             web.ctx.start_response()
             return [response,]
         # No request handler!
         # 404 Not found
         web.ctx.status = '404 Not Found'
-        web.ctx.headers = {'Content-Type': 'text/plain'}
+        web.ctx.headers['Content-Type'] = 'text/plain'
         web.ctx.start_response()
         return ['Not found',]
